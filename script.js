e6312cd4da9746d9687a535e313def813ffbfdd1
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-03-12 21:09:13 +0200

    Init

--- script.js
+++ script.js
@@ -149,6 +149,7 @@ taskLists.forEach(taskList =>
 		inputField.classList.add("task-description");
 		taskTitle.replaceWith(inputField);
 		inputField.focus();
+		inputField.setSelectionRange(currentTitle.length, currentTitle.length);
 
 		function handleEnterOrBlur() {
 			const newTitle = inputField.value;
@@ -262,7 +263,7 @@ function updateLocalStorage() {
 }
 
 //////////////////////////////////////////
-// Vertical sort
+// Dran and drop (with mouse)
 
 let dragging = null;
 
@@ -285,8 +286,10 @@ taskLists.forEach(task => {
 			const mouseY = event.clientY;
 			if (mouseY < draggedOverMiddleY) {
 				task.insertBefore(dragging, node);
+				return;
 			} else {
 				task.insertBefore(dragging, node.nextSibling);
+				return;
 			}
 		}
 	});
@@ -299,7 +302,7 @@ taskLists.forEach(task => {
 	});
 });
 
-// add event listeners for all sections
+// Add event listeners for all sections
 sections.forEach(section => {
 	section.addEventListener("dragover", function (e) {
 		e.preventDefault();
@@ -332,88 +335,83 @@ sections.forEach(section => {
 			const targetSection = event.target.closest(".section");
 			const taskList = targetSection.querySelector(".task-list");
 			if (taskList.childElementCount === 0) taskList.append(task);
-
-			updateLocalStorage();
 		}
+		updateLocalStorage();
 	});
 });
 
 //////////////////////////////////////////
 // Touch screens
-
-// Add touch event listeners to draggable items
-taskLists.forEach(draggableList => {
-	// Touch event handler for starting a drag
-	draggableList.addEventListener("touchstart", touchStart);
-
-	// Touch event handler for ending a drag
-	draggableList.addEventListener("touchend", touchEnd);
-});
+//////////////////////////////////////////
 
 // Touch event handler functions
 let targetSection;
 let touchStartX, touchStartY;
-let draggedItem;
 
-function touchStart(event) {
-	draggedItem = event.target.closest(".task");
+// Add touch event listeners to draggable items
+taskLists.forEach(task => {
+	// Touch event handler for starting a drag
+	task.addEventListener("touchstart", function (event) {
+		console.log("click");
 
-	// Store the initial touch coordinates
-	touchStartX = event.touches[0].clientX;
-	touchStartY = event.touches[0].clientY;
+		if (event.target.nodeName === "INPUT") return;
+		if (event.target.classList.contains("icon")) return;
 
-	// Add dragging class to dragged item
-	draggedItem.classList.add("dragging");
-}
+		dragging = event.target.closest(".task");
 
-function touchEnd(event) {
-	// Find the section where the drag ended
-	let touch = event.changedTouches[0];
-	let minDistance = Infinity;
-	for (let i = 0; i < sections.length; i++) {
-		let section = sections[i];
-		let rect = section.getBoundingClientRect();
-		let distance = Math.hypot(
-			touch.clientX - rect.left - rect.width / 2,
-			touch.clientY - rect.top - rect.height / 2
-		);
-		if (distance < minDistance) {
-			minDistance = distance;
-			targetSection = section;
+		// Store the initial touch coordinates
+		touchStartX = event.touches[0].clientX;
+		touchStartY = event.touches[0].clientY;
+
+		// Add dragging class to dragged item
+		dragging.classList.add("dragging");
+	});
+
+	// Touch event handler for ending a drag
+	task.addEventListener("touchend", function (event) {
+		// console.log("click");
+
+		// Find the section where the drag ended
+		let touch = event.changedTouches[0];
+		let minDistance = Infinity;
+		for (let i = 0; i < sections.length; i++) {
+			let section = sections[i];
+			let rect = section.getBoundingClientRect();
+			let distance = Math.hypot(
+				touch.clientX - rect.left - rect.width / 2,
+				touch.clientY - rect.top - rect.height / 2
+			);
+			if (distance < minDistance) {
+				minDistance = distance;
+				targetSection = section;
+			}
 		}
-	}
 
-	// Remove dragging class from dragged item
-	draggedItem.classList.remove("dragging");
-	event.target.closest(".section").classList.remove("drag-over");
+		// Remove dragging class from dragged item
+		if (!dragging) return;
+		dragging.classList.remove("dragging");
+		event.target.closest(".section").classList.remove("drag-over");
 
-	// Check if the target section is valid and has a task list
-	if (targetSection && targetSection.querySelector(".task-list")) {
 		// Move the dragged item to the target section's task list
-		targetSection.querySelector(".task-list").appendChild(draggedItem);
+		targetSection.querySelector(".task-list").appendChild(dragging);
 
 		// Return to normal styles
 		sections.forEach(section => section.classList.remove("drag-over"));
-		draggedItem.style.transform = `translate(0, 0)`;
+		dragging.style.transform = `translate(0, 0)`;
 
-		draggedItem = null;
+		dragging = null;
 
-		// Update local storage
 		updateLocalStorage();
-	}
-}
+	});
+});
 
 // Add touch event listeners to sections for drag over and drag leave
 sections.forEach(section => {
-	// Touch event handler for when a draggable item is over a section
 	section.addEventListener("touchmove", touchMove);
-
-	// Touch event handler for when a draggable item leaves a section
 	section.addEventListener("touchleave", touchLeave);
 });
 
 function touchMove(event) {
-	// Prevent default touch event behavior
 	event.preventDefault();
 
 	// Finde closest section to movement
@@ -444,8 +442,9 @@ function touchMove(event) {
 	const deltaX = touchX - touchStartX;
 	const deltaY = touchY - touchStartY;
 
+	if (!dragging) return;
 	// Move the dragged item using CSS transform
-	draggedItem.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+	dragging.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
 
 	// Add drag-over class to the section
 	targetSection.classList.add("drag-over");
@@ -458,3 +457,95 @@ function touchLeave(event) {
 }
 
 //////////////////////////////////////////
+
+let currentLi = null;
+let initialPos = null;
+let currentPos = null;
+let diff = 0;
+isTouchMoving = false;
+
+function handleTouchStart(e) {
+	if (e.target.nodeName === "INPUT") return;
+	if (e.target.classList.contains("icon")) return;
+
+	currentLi = e.target.closest(".task");
+	if (!currentLi) return;
+
+	initialPos = e.touches[0].clientY;
+	isTouchMoving = false;
+}
+
+function handleTouchMove(e) {
+	e.preventDefault();
+
+	if (e.target.nodeName === "INPUT") return;
+	if (e.target.classList.contains("icon")) return;
+
+	if (!currentLi) return;
+
+	currentPos = e.touches[0].clientY;
+	diff = currentPos - initialPos;
+
+	if (Math.abs(diff) < 10) return;
+
+	isTouchMoving = true;
+
+	currentLi.style.transform = `translateY(${diff}px)`;
+
+	const ul = currentLi.closest(".task-list");
+	const liArray = [...ul.querySelectorAll(".task")];
+
+	for (const li of liArray) {
+		if (li === currentLi) continue;
+
+		const rect = li.getBoundingClientRect();
+		const mid = (rect.bottom + rect.top) / 2;
+
+		if (currentPos > mid) {
+			li.classList.add("hover-below");
+			li.classList.remove("hover-above");
+		} else {
+			li.classList.add("hover-above");
+			li.classList.remove("hover-below");
+		}
+	}
+}
+
+function handleTouchEnd() {
+	if (Math.abs(diff) < 10 || !isTouchMoving) return;
+	if (!currentLi) return;
+
+	currentLi.style.transform = "";
+
+	const ul = currentLi.closest(".task-list");
+	const liArray = [...ul.querySelectorAll(".task")];
+	const newIndex = liArray.indexOf(currentLi);
+
+	let i = 0;
+	for (const li of liArray) {
+		if (li === currentLi) continue;
+
+		const rect = li.getBoundingClientRect();
+		const mid = (rect.bottom + rect.top) / 2;
+
+		if (currentPos > mid && newIndex > i) i++;
+		if (currentPos < mid && newIndex < i) i--;
+	}
+
+	ul.insertBefore(currentLi, liArray[i]);
+
+	document
+		.querySelectorAll(".task")
+		.forEach(t => t.classList.remove("hover-above", "hover-below"));
+
+	currentLi = null;
+	initialPos = null;
+	currentPos = null;
+	diff = 0;
+}
+
+taskLists.forEach(taskList => {
+	taskList.addEventListener("touchstart", handleTouchStart);
+	taskList.addEventListener("touchmove", handleTouchMove);
+	taskList.addEventListener("touchend", handleTouchEnd);
+});
